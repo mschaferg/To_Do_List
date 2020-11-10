f71d9173c4a3f1bd2d326a12e296fc89f899a276
mschaferg <mschaferg@users.noreply.github.com>
2020-11-10 15:55:08 -0500

    Initial Commit

---
+++ To_Do_List.js
@@ -0,0 +1,29 @@
+let myToDoList = [];
+
+showList = () => {
+    var x = document.getElementById("listParent");
+    if (x.style.visibility === "hidden") {
+        x.style.visibility = "visible";
+    } else {
+        x.style.visibility = "visible";
+    };
+};
+
+addTask = () => {
+    showList();
+
+    var newTask = document.getElementById("newTask").value;
+    var node = document.createElement("p");
+    var textnode = document.createTextNode(newTask);
+    node.appendChild(textnode);
+    document.getElementById("toDoList").appendChild(node);
+
+    var removeTask = document.createElement('input');
+    removeTask.setAttribute('type', 'button');
+    removeTask.setAttribute("value", "❌");
+    removeTask.setAttribute("id", "removeButton");
+    removeTask.addEventListener('click', function (e) {
+        node.parentNode.removeChild(node);
+    }, false);
+    node.appendChild(removeTask);
+};
